78bcb4e860e88f76974955ee5a2b61487be68fdb
Jorge Diaz <jorlup@gmail.com>
2022-08-20 04:22:45 -0500

    fix: eliminados los mensajes

--- src/app/components/auth/registro/registro.component.ts
+++ src/app/components/auth/registro/registro.component.ts
@@ -2,27 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { AbstractControlOptions, FormControl, FormGroup, Validators } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
 import { Store } from '@ngrx/store';
-import { MessageService } from 'primeng/api';
 import { sexoEmbarazo } from 'src/app/helpers/validation';
 import { Registro } from 'src/app/models/user.interface';
 import { SignUp } from 'src/app/store/actions/auth.actions';
+import { ResetUser } from 'src/app/store/actions/user.actions';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.scss'],
-  providers: [MessageService],
 })
 export class RegistroComponent implements OnInit {
   constructor(
     private store$: Store,
-    private fb: FormBuilder,
-    private messageService: MessageService
+    private fb: FormBuilder
   ) {}
 
-  ngOnInit(): void {}
-
-  
+  ngOnInit(): void {
+    // se borra en caso de un nuevo registro, similar a un logout
+    localStorage.removeItem('accessToken');
+    this.store$.dispatch(ResetUser());
+  }
 
   registroForm = this.fb.group(
     {
@@ -40,7 +40,6 @@ export class RegistroComponent implements OnInit {
   registro(): void {
     console.log(this.registroForm.value);
     console.log('store', this.store$);
-    //this.messageService.add({severity:'success', summary: 'Success', detail: JSON.stringify(this.registroForm.value)});
     this.store$.dispatch(
       SignUp({ params: this.registroForm.value as Registro })
     );
--- src/app/components/dashboard/profile/profile.component.ts
+++ src/app/components/dashboard/profile/profile.component.ts
@@ -1,29 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { select, Store } from '@ngrx/store';
-import { AuthService } from 'src/app/services/auth.service';
 import { ResetUser } from 'src/app/store/actions/user.actions';
 import { isUserLoading, userData } from 'src/app/store/selectors/user.selectors';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
 
-  constructor(private authService: AuthService, private store$: Store, private router: Router) { }
+  constructor(private store$: Store, private router: Router) { }
 
   globalLoading$ = this.store$.pipe(select(isUserLoading));
   userData$ = this.store$.select(userData);
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
   onSalir() {
     localStorage.removeItem('accessToken');
     this.store$.dispatch(ResetUser());
     this.router.navigateByUrl('/registro').then();
   }
-
 }
